482ed0a8ad4e53a52d9e715221cf92a98c193335
BryanValc <epicspartanryan@gmail.com>
2026-08-21 05:08:17 -0600

    Bundled celery, celerybeat, flower, properly integrated within docker-compose

--- app/main.py
+++ app/main.py
@@ -1,12 +1,29 @@
+from celery import Celery
 from fastapi import FastAPI
+import os
+
+BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://dragonfly:6379/0")
+RESULT_URL = os.getenv("CELERY_RESULT_BACKEND", "redis://dragonfly:6379/0")
 
 app = FastAPI()
 
+celery = Celery(
+    __name__,
+    broker=BROKER_URL,
+    backend=RESULT_URL
+)
+
+
+@celery.task
+def divide(x, y):
+    import time
+    time.sleep(5)
+    return x / y
+
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    task = divide.delay(1, 2)
+    return {"message": "Hello World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
